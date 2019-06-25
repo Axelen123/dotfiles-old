0edb67ea55ddb061f4b7a4806fe5ebe2e3d0b214
Axelen123 <axelakesson05@hotmail.com>
2019-06-25 13:52:36 +0200

    Fix GOPATH in env

--- .zsh-custom/scripts/env.zsh
+++ .zsh-custom/scripts/env.zsh
@@ -1,6 +1,5 @@
 export NVM_DIR="$HOME/.nvm"
 export GO111MODULE=on
-export GOPATH="$HOME/go"
 export PATH="$PATH:$HOME/.rvm/bin:"
 source $HOME/.cargo/env
 
@@ -8,3 +7,4 @@ source $HOME/.cargo/env
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 [[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"
+export GOPATH="$HOME/go"
